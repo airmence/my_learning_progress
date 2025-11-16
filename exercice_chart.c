94bd907d6d7566136cd35674a2a302c854062f34
hermence <airmence@outlook.fr>
2025-11-16 23:29:02 +0100

    continuting the exercice chart

---
+++ exercice_chart.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int adding_chart(int chart[], int size_chart);
+double average_chart(int chart[], int size_chart);
+void copy_chart(int source[], int destination[], int size);
+void maximum_chart(int chart[], int size_chart, int value_max);
+
+int main(int argc, char *argv[]){
+	int chart[4] = {45, 5, 653, 3};
+	maximum_chart(chart[], 4, 50);
+	return 0;
+}
+
+int adding_chart(int chart[], int size_chart){
+	int result = 0;
+	for(int i = 0; i < size_chart; i++){
+		result += chart[i]; 
+	}
+	return result;
+}
+
+double average_chart(int chart[], int size_chart){
+	double result = 0.0;
+	for(int i = 0; i < size_chart; i++){
+		result += chart[i];
+	}
+
+	return result /= size_chart;
+}
+
+void copy_chart(int source[], int destination[], int size){
+	for(int i = 0; i < size; i++){
+		destination[i] = source[i];
+	}
+}
+
+void maximum_chart(int chart[], int size_chart, int value_max){
+	for(int i = 0; i < size_chart; i++){
+		if(value_max > chart[i]){
+			chart[i] = 0;
+		}
+		printf("%d\n", chart[]);
+	}
+}
