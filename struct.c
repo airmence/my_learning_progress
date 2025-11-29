aa89e325a045010d1a0ec5c17ce518f232c73518
hermence <airmence@outlook.fr>
2025-11-29 21:54:03 +0100

    affectation struct to a fonction

--- struct.c
+++ struct.c
@@ -7,12 +7,18 @@ typedef struct{
 	float price;
 }Book_t;
 
+void display_book(const book_t *book_ptr){
+	printf("The book %s\nWho was realesed %d\nIs only %lf\n", book_ptr->name, book_ptr->year_publishing, book_ptr->price);
+}
 
 int main(int argc, char *argv[]){
 	Book_t my_book;
+
 	strcpy(my_book.name, "the night of the blue moon");
 	my_book.price = 23.99;
 	my_book.year_publishing = 2015;
-	printf("The book %s\nWho was realesed %d\nIs only %lf\n",my_book.name, my_book.year_publishing, my_book.price);
+	
+	display_book(&my_book)
+	
 	return 0;
 }
