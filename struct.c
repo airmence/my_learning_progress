ce043005c3b5e6eb094d14e62c463905231e2b9a
hermence <airmence@outlook.fr>
2025-11-29 22:02:25 +0100

    fixing some bugs on my orthographe

--- struct.c
+++ struct.c
@@ -5,20 +5,20 @@ typedef struct{
 	char name[100];
 	int year_publishing;
 	float price;
-}Book_t;
+}book_t;
 
 void display_book(const book_t *book_ptr){
 	printf("The book %s\nWho was realesed %d\nIs only %lf\n", book_ptr->name, book_ptr->year_publishing, book_ptr->price);
 }
 
 int main(int argc, char *argv[]){
-	Book_t my_book;
+	book_t my_book;
 
 	strcpy(my_book.name, "the night of the blue moon");
 	my_book.price = 23.99;
 	my_book.year_publishing = 2015;
 	
-	display_book(&my_book)
+	display_book(&my_book);
 	
 	return 0;
 }
