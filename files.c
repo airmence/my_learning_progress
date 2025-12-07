afd8060fea1bc70f9f77cd8792e63fc4616cec26
hermence <airmence@outlook.fr>
2025-12-07 11:05:57 +0100

    my work on writting in files and reading

--- files.c
+++ files.c
@@ -1,32 +1,32 @@
 #include<stdio.h>
 
-void printf_files();
+void open_files(const char *files);
+void writingFiles(files);
 
 int main(int argc, char *argv[]){
+	open_files("text.txt");
+	return 0;
+}
 
+void open_files(const char *files_name){
+	
 	FILE* files = NULL;
-	files = fopen("text.txt", "r+");
-
+	files = fopen(files_name, "r+");
+	
 	if(files != NULL){
 		printf("files find open in read and write\n");
 	}
 
-	if(files == NULL){
-		files = fopen("text.txt", "w+");
+	else{
+		files = fopen(files_name, "w+");
 		printf("files not find creating a new file in read and write\n");
 	}
+
+	fprintf(files, "hello this is a file");
 
 	if(files != NULL){
 		printf("the files has been closed\n");
 		fclose(files);
 	}
-
-	else{
-		printf("files failed to open\n");
-	}
-	return 0;
 }
 
-void printf_files(){
-
-}
