2be0a0748a9c18482c01f3f99f8bd1acb84ccd37
hermence <hermence.airmence@outlook.fr>
2026-01-13 19:57:18 +0100

    finishing bank money exercice maybee upgrade it later on

--- exercice/bank-money.c
+++ exercice/bank-money.c
@@ -1,57 +1,30 @@
 #include<stdio.h>
-
 int main(int argc, char *argv[]){
 
 	int  asked_money = 0;
 	printf("How many money do you want :");
 	scanf("%d", &asked_money);
-
-	int billets_500 = 0, billets_200 = 0, billets_100 = 0, billets_50 = 0, billets_20 = 0, billets_10 = 0, billets_5 = 0;
-
-	billets_500 = asked_money/500;
-	asked_money = asked_money%500;
-
-	billets_200 = asked_money/200;
-	asked_money = asked_money%200;
 	
-	billets_100 = asked_money/100;
-	asked_money = asked_money%100;
+	if(asked_money <= 0){
+		printf("ERROR: invalid amount\n");
+		return 1;
+	}
 	
-	billets_50 = asked_money/50;
-	asked_money = asked_money%50;
-	
-	billets_20 = asked_money/20;
-	asked_money = asked_money%20;
-	
-	billets_10 = asked_money/10;
-	asked_money = asked_money%10;
-	
-	billets_5 = asked_money/5;
-	asked_money = asked_money%5;
+	int money_bill[] = {500, 200, 100, 50, 20, 10, 5};
+	int size_chart = sizeof(money_bill) / sizeof(money_bill[0]);
 
-	printf("im giving you :\n");
+	for(int i = 0; i < size_chart; i++){
 
-	if(billets_500 != 0){
-		printf("%d of 500 bill\t", billets_500);
+		int number_Bills = 0;
+		number_Bills = asked_money/money_bill[i];
+		asked_money = asked_money%money_bill[i];
+		
+		if(number_Bills != 0){
+			printf("i give you %d of %d bills\n", number_Bills, money_bill[i]);}
 	}
-	if(billets_200 != 0){
-		printf("%d of 200 bill\t", billets_200);
-	}
-	if(billets_100 != 0){
-		printf("%d of 100 bill\t", billets_100);
-	}
-	if(billets_50 != 0){
-		printf("%d of 50 bill\t", billets_50);
-	}
-	if(billets_20 != 0){
-		printf("%d of 20 bill\t", billets_20);
-	}
-	if(billets_10 != 0){
-		printf("%d of 10 bill\t", billets_10);
-	}
-	if(billets_5 != 0){
-		printf("%d of 5 bill\t", billets_5);
-	}
-	printf("\n");
+	
+	if(asked_money != 0){
+		printf("i can't give you the rest\n");}
 	return 0;
 }
+
